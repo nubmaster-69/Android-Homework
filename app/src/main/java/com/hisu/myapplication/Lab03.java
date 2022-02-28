4106efc88fe11985731ad93ece764a8a66e5decb
Harry.dev <chihieunguyen9a2@gmail.com>
2022-02-28 17:07:36 +0700

    Lab03

---
+++ app/src/main/java/com/hisu/myapplication/Lab03.java
@@ -0,0 +1,14 @@
+package com.hisu.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lab03 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab03);
+    }
+}
