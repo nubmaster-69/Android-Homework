8d214ca34c8652eca8010260c69e0649436ba13d
Harry.dev <chihieunguyen9a2@gmail.com>
2022-03-15 10:19:39 +0700

    Fix bug: ratingbar not clickable when change size

--- app/src/main/java/lab4/activity/ListViewActivity.java
+++ app/src/main/java/lab4/activity/ListViewActivity.java
@@ -26,16 +26,8 @@ public class ListViewActivity extends AppCompatActivity {
 
         ArrayList<Product> products = new ArrayList<>();
         products.add(new Product(R.mipmap.ca_nau_lau, "Cá nấu lẩu", "Shop Devang"));
-//        products.add(new Product(R.mipmap.ga_bo_toi, "1KG khô gà bơ tỏi", "Shop LTD Food"));
-//        products.add(new Product(R.mipmap.do_choi_dang_mo_hinh, "Đồ chơi mô hình", "Shop thế giới đồ chơi"));
-
-//        products.add(new Product(R.mipmap.ca_nau_lau, "Cá nấu lẩu", "Shop Devang"));
-//        products.add(new Product(R.mipmap.ga_bo_toi, "1KG khô gà bơ tỏi", "Shop LTD Food"));
-//        products.add(new Product(R.mipmap.do_choi_dang_mo_hinh, "Đồ chơi mô hình", "Shop thế giới đồ chơi"));
-//
-//        products.add(new Product(R.mipmap.ca_nau_lau, "Cá nấu lẩu", "Shop Devang"));
-//        products.add(new Product(R.mipmap.ga_bo_toi, "1KG khô gà bơ tỏi", "Shop LTD Food"));
-//        products.add(new Product(R.mipmap.do_choi_dang_mo_hinh, "Đồ chơi mô hình", "Shop thế giới đồ chơi"));
+        products.add(new Product(R.mipmap.ga_bo_toi, "1KG khô gà bơ tỏi", "Shop LTD Food"));
+        products.add(new Product(R.mipmap.do_choi_dang_mo_hinh, "Đồ chơi mô hình", "Shop thế giới đồ chơi"));
 
         ListView list = findViewById(R.id.myListView);
 
